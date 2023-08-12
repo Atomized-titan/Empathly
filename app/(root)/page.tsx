c8e80a7f94348ab73e42b5de742e917b968ba07f
Pushpal Ghoshal <1804252@kiit.ac.in>
2023-08-12 19:02:06 +0530

    feat: bottom bar | mobile

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,10 +3,10 @@ import { UserButton } from '@clerk/nextjs';
 export default function Home() {
   return (
     <main>
-      <h1 className='head-text'>Empathly</h1>
-      <div>
+      <h1 className='head-text'>Home</h1>
+      {/* <div>
         <UserButton afterSignOutUrl='/' />
-      </div>
+      </div> */}
     </main>
   );
 }
--- components/common/BottomBar.tsx
+++ components/common/BottomBar.tsx
@@ -1,5 +1,38 @@
+'use client';
+
+import { sidebarLinks } from '@/constants';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
 const BottomBar = () => {
-  return <div>BottomBar</div>;
+  const pathname = usePathname();
+
+  return (
+    <section className='bottombar'>
+      <div className='bottombar_container'>
+        {sidebarLinks.map((link) => {
+          const isActive =
+            (pathname.includes(link.route) && link.route.length > 1) ||
+            pathname === link.route;
+
+          return (
+            <Link
+              href={link.route}
+              key={link.label}
+              className={`bottombar_link text-light-1 ${
+                isActive && 'bg-primary'
+              }`}
+            >
+              <link.icon className='w-6 h-6' />
+              <p className='text-subtle-medium text-light-1 max-sm:hidden'>
+                {link.label.split(/\s+/)[0]}
+              </p>
+            </Link>
+          );
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default BottomBar;
