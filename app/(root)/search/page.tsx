b02414b4af6dc0c06e780a9b43d1be023e993e92
Saharsh Shukla <shukla.saharsh7.work@gmail.com>
2023-08-13 22:30:12 +0530

    feat: something

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -1,1 +1,19 @@
+import {fetchAllUsers} from "@/lib/actions/users.actions";
 
+async function Page() {
+    const allUsers = await fetchAllUsers("*", 1, 10)
+    console.log(allUsers);
+    return (
+        <main>
+            {allUsers.users.map((el) => {
+                return (
+                    <div key={1}>
+                        el.
+                    </div>
+                )
+            })}
+        </main>
+    )
+ }
+
+ export default Page;
